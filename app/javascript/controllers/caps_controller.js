7e2a67cae9c8fe45f64944326722cc26f10ed9b2
neuronadev <fwneurona@gmail.com>
2023-06-08 17:15:36 -0600

    eval

---
+++ app/javascript/controllers/caps_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="caps"
+export default class extends Controller {
+  connect() {}
+ 
+  repetidos(event){
+       var value = event.target.value
+       var itemscapsol = document.getElementsByClassName('itemcapsol')
+       var repetido = 0
+       if ( itemscapsol.length > 1 ){
+             for (let i = 0; i < itemscapsol.length; i++ ){
+                     if (itemscapsol[i].value == value){
+                            repetido ++;          
+                     }
+             }
+             if ( repetido > 1 ){
+                    event.target.value=''
+                    alert("El capitulo ya fue seleccionado previamente, no se peuden repetir.")
+             }
+        }   
+  } 
+
+}
