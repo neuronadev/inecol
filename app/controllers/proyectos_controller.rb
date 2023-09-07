5cbc0fea1ac64e7e41ca09b7b173fb14dd11896f
neuronadev <fwneurona@gmail.com>
2023-09-07 15:29:39 -0600

    up

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -128,7 +128,7 @@ class ProyectosController < ApplicationController
        Enlace.create!(proyecto_id:proyecto.id, enevento_id:enevento.id)
        ##enviar email al seguimiento de proyectos### 
          param_email={nproyecto: proyecto.nombre, responsable: proyecto.persona.nom_espacio }
-         data = Util::Email.proyecto_enlace("sara.sanchez@inecol.mx", "antonio.francisco@inecol.mx", param_email, "d-a588d4f9befe40cdb1aa4d32b02d20f4")
+         data = Util::Email.proyecto_enlace("antonio.francisco@inecol.mx", "antonio.francisco@inecol.mx", param_email, "d-a588d4f9befe40cdb1aa4d32b02d20f4")
        #########################  
        respond_to do |format|
            format.json { render json:@proyecto.to_json }
