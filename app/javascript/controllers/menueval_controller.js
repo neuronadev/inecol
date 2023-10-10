20ceb10ec8c000b7ebc4d50612ba67aee217b8e7
neuronadev <fwneurona@gmail.com>
2023-10-10 17:31:26 -0600

    up

---
+++ app/javascript/controllers/menueval_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="menueval"
+export default class extends Controller {
+  connect() { console.log("opciones menu") }
+  
+
+  clkOpcion(event){
+    var item_nom = this.actOpcion(event.target)
+    this.desacOpcion(item_nom)
+  }
+
+  actOpcion(item){
+      var c = item.closest("#itemopt")
+      c.classList.add("bg-gray-200")
+      c.classList.remove("hover:bg-gray-200", "hover:ring-2")
+    return c.dataset.nom 
+  }
+
+  desacOpcion(item_nom){
+      var elements = document.getElementById("items_menu").getElementsByClassName("itemmenu")
+      for (let i = 0; i < elements.length; i++) {
+            if (elements[i].dataset.nom != item_nom ){
+                  elements[i].classList.remove("bg-gray-200");  
+                  elements[i].classList.add("hover:bg-gray-200", "hover:ring-2")
+            }      
+      }
+  }
+  
+
+}
--- app/javascript/controllers/trix_controller.js
+++ app/javascript/controllers/trix_controller.js
@@ -32,7 +32,7 @@ export default class extends Controller {
 
   openForegroundColorPicker () {
     this.foregroundColorPickerTarget.click()
-    console.log(this.foregroundColorPickerTarget)
+    
   }
 
   openBackgroundColorPicker () {
