8cb3215790cfc8aa315228e7b1dad430328ca4b5
neuronadev <fwneurona@gmail.com>
2023-10-24 11:10:20 -0600

    up

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -11,14 +11,37 @@ class Persona < ApplicationRecord
   end
 
   def nom_espacio
-       nombre_mod_1 = (apaterno||'').capitalize() + ' ' + (amaterno||'').capitalize() + ' ' + (nombre||'').capitalize() 
-       palabras = nombre_mod_1.split(' ')
-       str_nom = ''
-       palabras.each do |palabra|
-	           str_nom += palabra.capitalize()
-             str_nom += ' '
-       end
+      n = (nombre||'')
+      p = (apaterno||'')
+      m = (amaterno||'')
+
+      str_nom = zanityTxt(n) + ' ' + zanityTxt(p) + ' ' + zanityTxt(m)
+       #nombre_mod_1 = (apaterno||'').capitalize() + ' ' + (amaterno||'').capitalize() + ' ' + (nombre||'').capitalize() 
+       #palabras = nombre_mod_1.split(' ')
+       #str_nom = ''
+       #palabras.each do |palabra|
+       # arry = palabra.split(' ')
+       #   if arry.length > 0
+       #          arry.each do |p|
+       #                 str_nom += p.capitalize()
+       #                 str_nom += ' '
+       #          end
+       #   else
+	     #      str_nom += palabra.capitalize()
+       #      str_nom += ' '
+       #   end   
+       #end
      return str_nom  
+  end
+
+  def zanityTxt(txt)
+    str = ''
+    ar_txt = txt.split(' ')
+      ar_txt.each do |n|
+          str += n.capitalize()
+          str += ' '
+      end
+    return str
   end
 
 end
