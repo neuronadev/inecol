d95a66d5a4ef0818f422daa08b68ee9aec547694
neuronadev <fwneurona@gmail.com>
2024-01-09 03:19:25 -0600

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  config.action_mailer.delivery_method = :smtp
+  #host = 'localhost:3000'
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => "sisproyectos.inecol@gmail.com",
+   :password             => "jjsiqqtiovqjifsz",
+   :authentication       => "plain",
+   :enable_starttls_auto => true,
+   :openssl_verify_mode => 'none'
+  }
   
 end
