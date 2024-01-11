4a36bc9fed167d50575524a7c44f0d0d44c3aefa
neuronadev <fwneurona@gmail.com>
2024-01-11 16:52:51 -0600

    up

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Inecol
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    #config.time_zone = :local
+    config.active_record.default_timezone = :local
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
      config.active_storage.replace_on_assign_to_many = false
