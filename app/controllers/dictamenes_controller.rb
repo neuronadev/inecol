4b7d25ebed15a0a5a7d32c907be0ae28e9108a79
neuronadev <fwneurona@gmail.com>
2024-01-16 14:52:10 -0600

    up

--- app/controllers/dictamenes_controller.rb
+++ app/controllers/dictamenes_controller.rb
@@ -39,6 +39,17 @@ class DictamenesController < ApplicationController
                                    <p>Favor de ingresar al sistema para firmar.</p>
                                    <p><b>Enlace: <a href='https://sisproyectos.inecol.edu.mx/'>Ingresar al Sistema de Proyectos Externos</a></b>" 
 
+                    current_time = Time.now
+                    tiempo = (current_time.to_f * 1000).to_i
+                    file_nm = "email_dictamen_#{tiempo.to_s}.txt"
+                    path = "log/#{file_nm}"
+                    File.open(path, 'w') do |file|
+                        file.write("<html><body style='font-size:15px;font-family: Arial, Helvetica, sans-serif;'>
+                                     #{@dictamen.txtdictamen}
+                                     <p><b>Enlace: <a href='https://sisproyectos.inecol.edu.mx/'>Ingresar al Sistema de Proyectos Externos</a></b>
+                                    </body></html>")
+                    end
+
                     p = @proyecto.persona
                     c = Cuenta.where(persona_id:p.id).first
                     u = Usuario.find(c.usuario_id) 
@@ -56,11 +67,6 @@ class DictamenesController < ApplicationController
                             end
                         end
                    end
-
-                    #current_time = Time.now
-                    #tiempo = (current_time.to_f * 1000).to_i
-                    #file_nm = "email_solfirma_#{tiempo.to_s}.txt"
-                    #path = "log/#{file_nm}"
                  
                     #COMITE
                     emails_comite.each do |ecom|
@@ -71,20 +77,20 @@ class DictamenesController < ApplicationController
 
                     #RESPONSBLE
                     Thread.new  {               
-                        `(sleep 15;echo "<html><body style='font-size:14px;font-family: Arial, Helvetica, sans-serif;'>#{@dictamen.txtdictamen}</body></html>" | mail -a "Content-Type: text/html; charset=UTF-8" -s "Dictamen de Proyecto-#{@proyecto.persona.nom_espacio}-#{@proyecto.nombre[0..20]}" #{m}) &`
+                        `(sleep 15;cat #{path} | mail -a "Content-Type: text/html; charset=UTF-8" -s "Dictamen de Proyecto-#{@proyecto.persona.nom_espacio}-#{@proyecto.nombre[0..20]}" sara.sanchez@inecol.mx,#{m}) &`
                     }
 
                     #OTROS
                     emails_otros.each do |eotro|
                         Thread.new  {               
-                            `(sleep 15;echo "<html><body style='font-size:14px;font-family: Arial, Helvetica, sans-serif;'>#{@dictamen.txtdictamen}</body></html>" | mail -a "Content-Type: text/html; charset=UTF-8" -s "Dictamen de Proyecto-#{@proyecto.persona.nom_espacio}-#{@proyecto.nombre[0..20]}" #{eotro}) &`
+                            `(sleep 15;cat #{path} | mail -a "Content-Type: text/html; charset=UTF-8" -s "Dictamen de Proyecto-#{@proyecto.persona.nom_espacio}-#{@proyecto.nombre[0..20]}" #{eotro}) &`
                          }
                     end 
 
                     #AREA PROYECTOS EXTERNOS
-                    Thread.new  {
-                        `(sleep 15;echo "<html><body style='font-size:14px;font-family: Arial, Helvetica, sans-serif;'>#{@dictamen.txtdictamen}</body></html>" | mail -a "Content-Type: text/html; charset=UTF-8" -s "Dictamen de Proyecto-#{@proyecto.persona.nom_espacio}-#{@proyecto.nombre[0..20]}" sara.sanchez@inecol.mx) &`
-                    }
+                    #Thread.new  {
+                    #    `(sleep 15;cat #{path} | mail -a "Content-Type: text/html; charset=UTF-8" -s "Dictamen de Proyecto-#{@proyecto.persona.nom_espacio}-#{@proyecto.nombre[0..20]}" sara.sanchez@inecol.mx) &`
+                    #}
                    
                     format.html { redirect_to dictamen_path(@dictamen) } 
             else
