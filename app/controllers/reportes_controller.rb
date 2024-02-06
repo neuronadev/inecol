e301b072c2ca7e580ad1275dd58b07ec5f28e12c
neuronadev <fwneurona@gmail.com>
2024-02-06 12:57:40 -0600

    up

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -1,4 +1,5 @@
 class ReportesController < ApplicationController
+  layout :select_layout
   def index
   end
 
@@ -12,6 +13,24 @@ class ReportesController < ApplicationController
 
   def cconvenio
   end
+
+  protected 
+  def select_layout
+     
+     if !current_usuario.nil?
+          if current_usuario.cuenta.rol.clave == 'EL'
+                return 'enlaces' 
+          elsif current_usuario.cuenta.rol.clave == 'EVAL'
+                return 'validaciones'
+          elsif current_usuario.cuenta.rol.clave == 'PLAN'
+                return 'planeacion'
+          else
+                return 'proyectos'
+          end
+             
+     end
+
+ end        
 
 
 end
